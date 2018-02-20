1e6cc42ada163de63297b0af5ac79043c9456c47
James C. Davis <james@cos.io>
2018-02-20 14:00:26 -0500

    Add TypeScript Component

---
+++ tests/integration/components/app-ts-component/component-test.ts
@@ -0,0 +1,17 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import hbs from 'htmlbars-inline-precompile';
+
+module('Integration | Component | app-ts-component', function(hooks) {
+  setupRenderingTest(hooks);
+
+  test('it renders', async function(assert) {
+    // Set any properties with this.set('myProperty', 'value');
+    // Handle any actions with this.set('myAction', function(val) { ... });
+
+    await render(hbs`{{app-ts-component}}`);
+
+    assert.equal(this.element.textContent.trim(), 'App TS Component');
+  });
+});
